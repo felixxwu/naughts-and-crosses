59448bfaf7edb8437770eb7ef663f5906856d352
felixxwu <felixxwu@gmail.com>
2018-12-22 16:28:17 +0000

    try add db

--- index.js
+++ index.js
@@ -13,4 +13,30 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/selectAll', (req, res) => selectAll(req, res))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM groups');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+async function selectAll(req, res) {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM groups');
+    const results = { 'results': (result) ? result.rows : null};
+    res.send(results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+}
